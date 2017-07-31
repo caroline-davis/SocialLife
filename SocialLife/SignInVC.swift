239bc233fce3c638037021d8fbaf1cdb2a49864b
Caroline Davis <caroline_davis@live.com>
2017-07-31 17:26:39 +1000

    first commit

---
+++ SocialLife/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialLife
+//
+//  Created by Caroline Davis on 31/07/2017.
+//  Copyright © 2017 Caroline Davis. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
